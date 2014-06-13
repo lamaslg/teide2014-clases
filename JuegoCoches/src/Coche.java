8f84f4b178eb2b52c501974d0f28baf17eb30c1c
Luis Gil <lamaslg@gmail.com>
2014-06-13 16:56:18 +0200

    Primera subida

---
+++ JuegoCoches/src/Coche.java
@@ -0,0 +1,8 @@
+
+public class Coche {
+
+	public String color;
+	public String combustible;
+	public int deposito;
+	public double consumo;
+}
---
+++ JuegoCoches/src/Juego.java
@@ -0,0 +1,13 @@
+
+public class Juego {
+
+	public static void main(String[] args) {
+		
+		Coche c1=new Coche();
+		c1.color="Rojo";
+		
+		
+		System.out.println("El coche es "+c1.color);
+			}
+	
+}
