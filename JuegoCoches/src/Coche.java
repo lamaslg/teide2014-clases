e9706c24fc9942f40a20e480b6e099f8d2922b2a
Luis Gil <lamaslg@gmail.com>
2014-06-13 19:43:28 +0200

    segunda

--- JuegoCoches/src/Coche.java
+++ JuegoCoches/src/Coche.java
@@ -1,8 +1,72 @@
 
 public class Coche {
 
-	public String color;
-	public String combustible;
-	public int deposito;
-	public double consumo;
+	//Atributos
+	private String color;
+	private String combustible;
+	private int deposito;
+	private double consumo;
+	
+	public Coche(){
+		color="nada";
+		combustible="nada";
+		deposito=0;
+		consumo=0;
+		
+	}
+	
+	//Creamos el constructor que permite recibir los argumentos del objeto
+	public Coche(String _color,String _combustible,int _deposito,double _consumo){
+		color=_color;
+		combustible=_combustible;
+		deposito=_deposito;
+		consumo=_consumo;
+		
+	}
+	//Setters y Getters
+	public String getColor(){
+		return color;
+		
+	}
+	public void setColor(String arg){
+		color=arg;
+	}
+	
+	public String getCombustible() {
+		return combustible;
+	}
+	public void setCombustible(String combustible) {
+		this.combustible = combustible;
+	}
+	public int getDeposito() {
+		return deposito;
+	}
+	public void setDeposito(int deposito) {
+		this.deposito = deposito;
+	}
+	public double getConsumo() {
+		return consumo;
+	}
+	public void setConsumo(double consumo) {
+		this.consumo = consumo;
+	}
+	public double consumido(double kms){
+		//como tengo litros por 100 km, calculo cuantas unidades de 100 km recorridas
+		double kmsrec=kms/100;
+		
+		
+		return kmsrec*consumo;		
+	}
+	public double consumido(double kms,double precio){
+		//Quiero lo que cuesta el trayecto, le doy km y precio litro
+		double kmsrec=kms/100;
+		
+		
+		return kmsrec*consumo*precio;		
+	}
+	public void  imprimir() {
+		System.out.println("Coche "+ color+ " consume "+consumo);
+		
+	}
+	
 }
--- JuegoCoches/src/Juego.java
+++ JuegoCoches/src/Juego.java
@@ -3,11 +3,18 @@ public class Juego {
 
 	public static void main(String[] args) {
 		
-		Coche c1=new Coche();
-		c1.color="Rojo";
+		Coche c1=new Coche("Naranja","Keroseno",125,5.5);
+		Coche c2=new Coche();
+		/*c1.setColor("Rojo");
+		c1.setConsumo(6.5);
+		*/
+		System.out.println("El coche es "+c1.getColor());
+		System.out.println(c2.getColor());
+		double cons=c1.consumido(23,1.4);
 		
+		System.out.println(cons);
 		
-		System.out.println("El coche es "+c1.color);
+		c1.imprimir();
 			}
 	
 }
